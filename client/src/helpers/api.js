3608f7b8504deb9cb5696d573f05ca7118b68863
dr-binaryV1 <developerdamian@gmail.com>
2017-10-24 01:34:48 -0500

    Create additional helper methods

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -10,6 +10,16 @@ export function submitSignUp(data) {
   });
 }
 
+export function submitSignIn(data) {
+  return fetch(`${url}/signin`,{
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify(data)
+  });
+}
+
 export function getSecret() {
   fetch(`${url}`, {
     headers: {
@@ -21,4 +31,26 @@ export function getSecret() {
   .then(res => res.json())
   .then(res => console.log(res))
   .catch(err => console.log(`Error reported: ${err}`))
+}
+
+export function getCartItems() {
+  return fetch(`${url}/shoppingcart`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'user': localStorage.getItem('user')
+    },
+    method: 'GET'
+  });
+}
+
+export function searchAmazon(amazonURL) {
+  return fetch(`${url}/search`,{
+    headers: {
+      'Content-Type': 'application/json',
+      'mode': 'cors',
+      'user': localStorage.getItem('user')
+    },
+    method: 'POST',
+    body: JSON.stringify(amazonURL)
+  });
 }
