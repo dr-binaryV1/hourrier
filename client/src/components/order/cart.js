b77d3c6e3a5271a5939230c32712d41d6ef6edfa
damian.wynter <damian.wynter@particularpresence.com>
2017-10-24 16:11:07 -0500

    Display items on cart page

--- client/src/components/order/cart.js
+++ client/src/components/order/cart.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import { getCartItems } from '../../helpers/api';
+import { getCart, getItems, deleteItem } from '../../helpers/api';
 
 class Cart extends Component {
   state = {
+    cartItemIds: [],
     cartItems: [],
     loading: false
   }
 
   componentDidMount() {
-    getCartItems()
+    this.getCartData();
+  }
+
+  getCartData() {
+    getCart()
+    .then(res => res.json())
+    .then(res => {
+      this.setState({ cartItemIds: res.itemIds });
+
+      getItems({ itemIds: this.state.cartItemIds })
       .then(res => res.json())
       .then(res => {
-        console.log(res);
-        this.setState({ cartItems: res.itemIds });
+        this.setState({ cartItems: res.items });
+      })
+      .catch(err => console.log(`Error reported: ${err}`));
+    })
+    .catch(err => console.log(`Error reported: ${err}`));
+  }
+  
+  onDeleteItem(id) {
+    deleteItem({itemId: id})
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ cartItemIds: res });
+        this.getCartData();
       })
       .catch(err => console.log(`Error reported: ${err}`));
   }
@@ -21,10 +42,33 @@ class Cart extends Component {
     return (
       <div className="container">
         {
-          this.state.cartItems.length < 1 ?
+          this.state.cartItemIds.length < 1 ?
           <h3>Shopping Cart Empty!</h3>
           :
-          ''
+          <ul>
+            {
+              this.state.cartItems.map(item => {
+                return <li key={item._id}>
+                <div className="row card">
+                  <div className="col s2">
+                    <img src={item.image} width="60" alt={item.name} />
+                  </div>
+                  <div className="col s7">
+                    {item.name}
+                    <div className="row">
+                      <div className=""><h6>Price: <b>{item.price}</b></h6></div>
+                    </div>
+                  </div>
+                  <div className="col s3">
+                    <button
+                      onClick={() => this.onDeleteItem(item._id)}
+                      className="waves-effect waves-light btn">Remove</button>
+                  </div>
+                </div>
+                </li>
+              })
+            }
+          </ul>
         }
       </div>
     )
