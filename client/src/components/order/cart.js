6a39c9af11e0f52bc8ea3097ee4960af934a99ae
damian.wynter <damian.wynter@particularpresence.com>
2017-10-25 11:00:47 -0500

    Add Checkout feature to cart

--- client/src/components/order/cart.js
+++ client/src/components/order/cart.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
   getCart,
   getItems,
-  deleteItem
+  deleteItem,
+  checkout
 } from '../../helpers/api';
 import { Link } from 'react-router-dom';
 
@@ -39,6 +40,20 @@ class Cart extends Component {
     })
     .catch(err => console.log(`Error reported: ${err}`));
   }
+
+  onCheckoutItems() {
+    this.setState({ loading: true });
+    checkout({ itemIds: this.state.cartItemIds })
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ loading: false });
+        return res.orderSaved ? this.setState({ cartItemIds: [], cartItems: [] }) : '';
+      })
+      .catch(err => {
+        console.log(`Error reported: ${err}`);
+        this.setState({ loading: false });
+      });
+  }
   
   onDeleteItem(id) {
     this.setState({ loading: true });
@@ -72,35 +87,44 @@ class Cart extends Component {
           this.state.cartItemIds.length < 1 ?
           <h3>Shopping Cart Empty!</h3>
           :
-          <ul>
-            {
-              this.state.cartItems.map(item => {
-                return <li key={item._id}>
-                <div className="row card">
-                  <div className="col s2">
-                    <img src={item.image} width="60" alt={item.name} />
-                  </div>
-                  <div className="col s8">
-                    {item.name}
-                    <div className="row">
-                      <div className=""><h6>Price: <b>{item.price}</b></h6></div>
-                      <Link to={item.url} target="_blank">View Product</Link>
+          <div>
+            <ul>
+              {
+                this.state.cartItems.map(item => {
+                  return <li key={item._id}>
+                  <div className="row card">
+                    <div className="col s2">
+                      <img src={item.image} width="60" alt={item.name} />
+                    </div>
+                    <div className="col s8">
+                      {item.name}
+                      <div className="row">
+                        <div className=""><h6>Price: <b>{item.price}</b></h6></div>
+                        <Link to={item.url} target="_blank">View Product</Link>
+                      </div>
+                    </div>
+                    <div className="col s2">
+                      <button
+                        onClick={() => this.onDeleteItem(item._id)}
+                        className="waves-effect waves-light btn">Remove</button>
+                        <div className="row">
+                          <div className="col s6"><p>Qty: </p></div>
+                          <div className="col s6"><input id="qty" type="number" defaultValue={1} onChange={(e) => this.onQuantityChange(e.target.value)} /></div>
+                        </div>
                     </div>
                   </div>
-                  <div className="col s2">
-                    <button
-                      onClick={() => this.onDeleteItem(item._id)}
-                      className="waves-effect waves-light btn">Remove</button>
-                      <div className="row">
-                        <div className="col s6"><p>Qty: </p></div>
-                        <div className="col s6"><input id="qty" type="number" defaultValue={1} onChange={(e) => this.onQuantityChange(e.target.value)} /></div>
-                      </div>
-                  </div>
-                </div>
-                </li>
-              })
-            }
-          </ul>
+                  </li>
+                })
+              }
+            </ul>
+            <div className="row">
+              <button
+                onClick={this.onCheckoutItems.bind(this)}
+                className="waves-effect waves-light btn"
+                id="checkout-btn">Checkout
+              </button>
+            </div>
+          </div>
         }
       </div>
     )
