8fe316486f0690afcf839ea1b98198aebc9b1dc0
dr-binaryV1 <developerdamian@gmail.com>
2017-10-26 00:10:23 -0500

    Created profile component

---
+++ client/src/components/user/profile.js
@@ -0,0 +1,102 @@
+import React, {Component } from 'react';
+import { getUser } from '../../helpers/api';
+
+import EditProfile from './edit_profile';
+
+class Profile extends Component {
+  state = {
+    user: null,
+    editingProfile: false
+  }
+
+  componentDidMount() {
+    getUser()
+      .then(res => res.json())
+      .then(res => this.setState({ user: res }))
+      .catch(err => console.log(`Error reported: ${err}`));
+  }
+
+  editComplete() {
+    this.setState({ editingProfile: false });
+    getUser()
+    .then(res => res.json())
+    .then(res => this.setState({ user: res }))
+    .catch(err => console.log(`Error reported: ${err}`));
+  }
+
+  render() {
+    const { user, editingProfile } = this.state;
+
+    return (
+      <div>
+        {
+          user ?
+          editingProfile ?
+          <EditProfile user={user} done={this.editComplete.bind(this)} />
+          :
+        <div className="container container-padding">
+          <div className="row">
+            <div className="col s6">
+              <h5>User Profile</h5>
+            </div>
+            <div className="col s6 right-align search-btn">
+              <button
+                id="edit-profile"
+                onClick={() => this.setState({ editingProfile: true })}
+                className="waves-effect waves-light btn">Edit Profile</button>
+            </div>
+          </div>
+          <br />
+          <div className="card">
+            <div className="row">
+              <div className="col s4">
+                <h5>Username: {user.username}</h5>
+              </div>
+            </div>
+            <div className="row left-align">
+              <div className="col s4">
+                <h6>First Name: {user.firstname}</h6>
+              </div>
+
+              <div className="col s4">
+                <h6>Last Name: {user.lastname}</h6>
+              </div>
+
+              <div className="col s4">
+                <h6>Email: {user.email}</h6>
+              </div>
+            </div>
+            <div className="row left-align">
+              <div className="col s6">
+                <h6>Mailing Address 1: {user.mailingAddress1}</h6>
+              </div>
+            </div>
+            <div className="row left-align">
+              <div className="col s6">
+                <h6>Mailing Address 2: {user.mailingAddress2}</h6>
+              </div>
+            </div>
+            <div className="row left-align">
+              <div className="col s4">
+                <h6>Mailing City: {user.mailingCity}</h6>
+              </div>
+
+              <div className="col s4">
+                <h6>Mailing Country: {user.mailingCountry}</h6>
+              </div>
+
+              <div className="col s4">
+                <h6>Mailing Zip: {user.mailingZip}</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        :
+        ''
+        }
+      </div>
+    )
+  }
+}
+
+export default Profile;
