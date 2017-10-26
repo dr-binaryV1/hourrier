d78b5df18efc90c8f6629d7907ff59f92bc61988
damian.wynter <damian.wynter@particularpresence.com>
2017-10-26 13:36:52 -0500

    Removed Redux thunk

---
+++ client/src/components/user/travel_itinerary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TravelItinerary = props => {
+  return (
+    <div className="container">
+      <div className="row card">
+        
+      </div>
+    </div>
+  )
+}
+
+export default TravelItinerary;
