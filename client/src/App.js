d029855f8ec94a198606ca72e53d25d0699f01e0
dr-binaryV1 <developerdamian@gmail.com>
2017-10-26 00:08:33 -0500

    Add Profile Path to App

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import SignIn from './components/auth/signin';
 import Cart from './components/order/cart';
 import { getSecret } from './helpers/api'
 import Header from './components/header';
+import Profile from './components/user/profile';
 import Home from './components/home';
 
 class App extends Component {
@@ -22,6 +23,7 @@ class App extends Component {
         <Route exact path="/shopping-cart" component={Cart} />
         <Route exact path="/sign-in" component={SignIn} />
         <Route exact path="/sign-up" component={SignUp} />
+        <Route exact path="/profile" component={Profile} />
       </div>
     );
   }
