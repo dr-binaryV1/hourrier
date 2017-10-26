f923e220f909779947efc80d989d778b95ec1d4c
damian.wynter <damian.wynter@particularpresence.com>
2017-10-26 13:35:40 -0500

    Remove redux thunk middleware

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import ReduxThunk from 'redux-thunk';
 import reducer from './reducers';
 import './index.css';
 import App from './App';
@@ -10,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Provider store={createStore(reducer, {}, applyMiddleware(ReduxThunk))}>
+  <Provider store={createStore(reducer, {}, applyMiddleware())}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
