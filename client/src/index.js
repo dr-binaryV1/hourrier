adb4cbeac61d2d55dbb2142178f70ddc301087b1
dr-binaryV1 <developerdamian@gmail.com>
2017-10-26 00:09:07 -0500

    Hook up app to redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import reducer from './reducers';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={createStore(reducer, {}, applyMiddleware(ReduxThunk))}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 , document.getElementById('root'));
 registerServiceWorker();
