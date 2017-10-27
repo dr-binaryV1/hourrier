1430a6b28be96a85e790d88e1d3146a2f996f256
dr-binaryV1 <developerdamian@gmail.com>
2017-10-27 08:09:41 -0500

    Add state and shipping form inputs

--- client/src/components/user/addShipping.js
+++ client/src/components/user/addShipping.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { addShipping } from '../../helpers/api';
+import { ProgressBar } from 'react-materialize';
 
 class AddShipping extends Component {
   state = {
@@ -6,7 +8,8 @@ class AddShipping extends Component {
     shippingAddress2: '',
     shippingCity: '',
     shippingCountry: '',
-    shippingZip: ''
+    shippingZip: '',
+    loading: false
   }
 
   renderInput(type, id, val, onchange) {
@@ -20,26 +23,110 @@ class AddShipping extends Component {
     )
   }
 
+  onSubmitShipping() {
+    this.setState({ loading: true });
+    const newAddress = {
+      shippingAddress1: this.state.shippingAddress1,
+      shippingAddress2: this.state.shippingAddress2,
+      shippingCity: this.state.shippingCity,
+      shippingCountry: this.state.shippingCountry,
+      shippingZip: this.state.shippingZip
+    };
+
+    addShipping(newAddress)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ loading: false });
+      })
+      .catch(err => {
+        console.log(`Error reported: ${err}`);
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div>
+
+        {
+          this.state.loading ? <ProgressBar /> : ''
+        }
+
         <div className="row left-align container-padding">
           <div className="col s12">
             <h5>Add Shipping</h5>
           </div>
         </div>
 
-        <div className="row card container-padding">
-          <div className="input-field col s4">
-            {
-              this.renderInput(
-                "text",
-                "shippingAddress1",
-                this.state.shippingAddress1,
-                (e) => this.setState({ shippingAddress1: e.target.value })
-              )
-            }
-            <label htmlFor="shippingAddress1">Shipping Address 1</label>
+        <div className="row container-padding">
+          <div className="row">
+            <div className="input-field col s4">
+              {
+                this.renderInput(
+                  "text",
+                  "shippingAddress1",
+                  this.state.shippingAddress1,
+                  (e) => this.setState({ shippingAddress1: e.target.value })
+                )
+              }
+              <label htmlFor="shippingAddress1">Shipping Address 1</label>
+            </div>
+            <div className="input-field col s4">
+              {
+                this.renderInput(
+                  "text",
+                  "shippingAddress2",
+                  this.state.shippingAddress2,
+                  (e) => this.setState({ shippingAddress2: e.target.value })
+                )
+              }
+              <label htmlFor="shippingAddress2">Shipping Address 2</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s4">
+              {
+                this.renderInput(
+                  "text",
+                  "shippingCity",
+                  this.state.shippingCity,
+                  (e) => this.setState({ shippingCity: e.target.value })
+                )
+              }
+              <label htmlFor="shippingCity">Shipping City</label>
+            </div>
+            <div className="input-field col s4">
+              {
+                this.renderInput(
+                  "text",
+                  "shippingCountry",
+                  this.state.shippingCountry,
+                  (e) => this.setState({ shippingCountry: e.target.value })
+                )
+              }
+              <label htmlFor="shippingCountry">Shipping Country</label>
+            </div>
+            <div className="input-field col s4">
+              {
+                this.renderInput(
+                  "text",
+                  "shippingZip",
+                  this.state.shippingZip,
+                  (e) => this.setState({ shippingZip: e.target.value })
+                )
+              }
+              <label htmlFor="shippingZip">Shipping Zip</label>
+            </div>
+          </div>
+          <div className="row">
+            <button
+              onClick={this.onSubmitShipping.bind(this)}
+              className="waves-effect waves-light btn"
+              id="add-shipping">Save</button>
+
+            <button
+            className="waves-effect waves-light btn"
+            id="cancel-add-shipping">Cancel</button>
           </div>
         </div>
       </div>
