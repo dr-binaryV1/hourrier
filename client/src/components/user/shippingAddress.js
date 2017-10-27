6d265e4170f926b5f7d397496fb32391bee65aaf
dr-binaryV1 <developerdamian@gmail.com>
2017-10-27 08:10:43 -0500

    Created Shipping Address component

---
+++ client/src/components/user/shippingAddress.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Button } from 'react-materialize';
+
+const ShippingAddress = props => {
+  const { address } = props;
+
+  return (
+    <div className="row card">
+      <div className="row">
+        <div className="col s4 left-align">
+          <p>Shipping Address 1: {address.shippingAddress1}</p>
+        </div>
+        <div className="col s4 left-align">
+          <p>Shipping Address 2: {address.shippingAddress2}</p>
+        </div>
+        <div className="col s4 right-align">
+          <Button waves='light'>Use</Button>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col s4 left-align">
+          <p>Shipping City: {address.shippingCity}</p>
+        </div>
+        <div className="col s4 left-align">
+          <p>Shipping Country: {address.shippingCountry}</p>
+        </div>
+        <div className="col s4 left-align">
+          <p>Shipping Zip: {address.shippingZip}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ShippingAddress;
