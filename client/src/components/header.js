7c1180be77735ea173b5f8be0456dd09b53b19b8
dr-binaryV1 <developerdamian@gmail.com>
2017-10-28 00:44:57 -0500

    Hook up header to redux to keep track of shoping cart item count

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { get_shopping_cart } from '../actions';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import ShoppingCart from 'react-icons/lib/md/shopping-cart';
+import Profile from 'react-icons/lib/md/account-circle';
 
-const Header = (props) => {
-  return (
-    <div className="navbar-fixed">
-    <nav>
-      <div className="nav-wrapper">
-        <Link to="/" className="brand-logo">Hourrier</Link>
-        <ul className="right hide-on-med-and-down">
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/shopping-cart">Cart</Link></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-  )
+class Header extends Component {
+  componentDidMount() {
+    this.props.get_shopping_cart();
+  }
+
+  render() {
+    return (
+      <div className="navbar-fixed">
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo">Hourrier</Link>
+          <ul className="right hide-on-med-and-down">
+            <li><Link to="/profile"><Profile size={30} /></Link></li>
+            <li>
+              <Link
+                to="/shopping-cart">
+                  <ShoppingCart size={30} />
+                ({ this.props.cartIds ? this.props.cartIds.length : 0 })
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    )
+  }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    cartIds: state.cartIds
+  }
+}
+
+export default connect(mapStateToProps, { get_shopping_cart })(Header);
