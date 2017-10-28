3354cd4cf7cd0cb8491043182a6486faddf2c911
dr-binaryV1 <developerdamian@gmail.com>
2017-10-28 00:44:37 -0500

    Create action types

---
+++ client/src/actions/types.js
@@ -0,0 +1,7 @@
+export const GET_USER = "get_user";
+export const GET_CART_IDs = "get_cart_ids";
+export const GET_CART_ITEMS = "get_cart_items";
+export const UPDATE_USER = "update_user";
+export const GET_ADDRESS = "get_address";
+export const DELETE_ADDRESS = "delete_address";
+export const DELETE_CART_ITEM = "delete_cart_item";
