580c39709df8d273f194195c192ef2f025ab54e7
dr-binaryV1 <developerdamian@gmail.com>
2017-10-28 00:48:38 -0500

    Handle actions with reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,22 @@
+import {
+  GET_USER,
+  GET_CART_IDs,
+  GET_CART_ITEMS
+} from '../actions/types';
+
 function reducer(state = {}, action) {
+  const { user, cartIds, cartItems } = action;
+
   switch(action.type) {
+    case GET_USER:
+      return { ...state, user };
+
+    case GET_CART_IDs:
+      return { ...state, cartIds };
+
+    case GET_CART_ITEMS:
+      return { ...state, cartItems };
+
     default:
       return state;
   }
