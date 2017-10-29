815656e65032cd4348ed2eb05d6d3ce0b49755c9
dr-binaryV1 <developerdamian@gmail.com>
2017-10-29 15:41:15 -0500

    Add require auth to protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,52 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Route, withRouter } from 'react-router-dom';
+import { get_authenticated_state } from './actions';
 import './App.css';
 
 import SignUp from './components/auth/signup';
 import SignIn from './components/auth/signin';
+import SignOut from './components/auth/signout';
 import Cart from './components/order/cart';
-import { getSecret } from './helpers/api'
 import Header from './components/header';
 import Profile from './components/user/profile';
 import Home from './components/home';
+import RequireAuth from './hoc/requireAuth';
 
 class App extends Component {
   componentDidMount() {
-    getSecret();
+    const token = localStorage.getItem('token');
+    token ? this.props.get_authenticated_state(true) : this.props.get_authenticated_state(false);
   }
 
   render() {
+    const { authenticated } = this.props;
     return (
       <div className="App">
-        <Header />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/shopping-cart" component={Cart} />
-        <Route exact path="/sign-in" component={SignIn} />
-        <Route exact path="/sign-up" component={SignUp} />
-        <Route exact path="/profile" component={Profile} />
+        {
+          authenticated === true || authenticated === false ?
+        <div>
+          <Header />
+          <Route exact path="/" component={RequireAuth(Home)} />
+          <Route exact path="/shopping-cart" component={RequireAuth(Cart)} />
+          <Route exact path="/sign-in" component={SignIn} />
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/sign-out" component={SignOut} />
+          <Route exact path="/profile" component={RequireAuth(Profile)} />
+        </div>
+        :
+        ''
+        }
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    authenticated: state.authenticated
+  }
+}
+
+export default withRouter(connect(mapStateToProps, { get_authenticated_state })(App));
+//export default App;
