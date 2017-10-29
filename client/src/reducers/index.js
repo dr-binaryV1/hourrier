92f90cdb05a9c1fb7d9724c53d56a75d2604ea66
dr-binaryV1 <developerdamian@gmail.com>
2017-10-29 15:45:44 -0500

    Add Get auth type to reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,15 +2,19 @@ import {
   GET_USER,
   GET_CART_IDs,
   GET_CART_ITEMS,
-  GET_SHIPPING_DETAILS
+  GET_SHIPPING_DETAILS,
+  GET_AUTH
 } from '../actions/types';
 
 function reducer(state = {}, action) {
-  const { user, cartIds, cartItems, shippingAddresses } = action;
+  const { user, cartIds, cartItems, shippingAddresses, authenticated } = action;
 
   switch(action.type) {
     case GET_USER:
       return { ...state, user };
+
+    case GET_AUTH:
+      return { ...state, authenticated };
 
     case GET_CART_IDs:
       return { ...state, cartIds };
