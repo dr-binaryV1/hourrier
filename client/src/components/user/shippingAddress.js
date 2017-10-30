34d8b82337b05ed7dadd3bf68eda62e895a5c444
damian.wynter <damian.wynter@particularpresence.com>
2017-10-30 16:47:55 -0500

    Add Icons for UI

--- client/src/components/user/shippingAddress.js
+++ client/src/components/user/shippingAddress.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { delete_shipping_address, change_primary_shipping } from '../../actions';
 import { connect }from 'react-redux';
 import { Button } from 'react-materialize';
+import TrashBin from 'react-icons/lib/md/delete';
+import Check from 'react-icons/lib/md/check';
 
 class ShippingAddress extends Component {
   render() {
@@ -31,22 +33,33 @@ class ShippingAddress extends Component {
         </div>
 
         <div className="row">
-          <div className="col s4 right-align">
-            <Button
-              onClick={() => this.props.delete_shipping_address({addressId: address._id})}
-              className="red btn-spacing"
-              waves='light'>Remove</Button>
+          <div className="col s5 left-align">
             {
               user.primaryShippingAddress === address._id ?
-              <Button disabled={true} className="green" waves='light'>Primary</Button>
+              <Button
+                disabled={true}
+                className="green btn-spacing"
+                waves='light'>
+                <Check size={20} /> Primary
+              </Button>
               :
               <Button
                 onClick={() => this.props.change_primary_shipping({primaryShippingAddress: address._id})}
-                className="green"
+                className="green btn-spacing"
                 waves='light'>
-                Primary
+                <Check size={20} /> Primary
               </Button>
             }
+            <Button
+              onClick={() => this.props.delete_shipping_address({addressId: address._id})}
+              className="red"
+              waves='light'>
+              <TrashBin size={20} /> Remove
+            </Button>
+          </div>
+
+          <div className="col s7 left-align">
+            {/* <p><b>Successful set to primary shipping address.</b></p> */}
           </div>
         </div>
       </div>
