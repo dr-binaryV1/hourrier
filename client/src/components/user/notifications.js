8a0ed182047ec48df774da67cd7dc8aef442d2f5
damian.wynter <damian.wynter@particularpresence.com>
2017-11-08 15:40:40 -0500

    Created notifications page

---
+++ client/src/components/user/notifications.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from 'react-materialize';
+
+class Notification extends Component {
+  render() {
+    return (
+      <Container>
+        <h5>No Notifications at this time</h5>
+      </Container>
+    )
+  }
+}
+
+export default Notification;
