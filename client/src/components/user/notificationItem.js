7877c479fcd258762419e3fae8f80b73528c8762
damian.wynter <damian.wynter@particularpresence.com>
2017-11-09 15:07:14 -0500

    Add method to display Items

--- client/src/components/user/notificationItem.js
+++ client/src/components/user/notificationItem.js
@@ -1,35 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, Row, Col, Button } from 'react-materialize';
+import { withRouter } from 'react-router-dom';
 
-const NotificationItem = (props) => {
-  return (
-    <Card className="left-align">
-      <Row>
-        <Col s={2}>
-          <img src="images/package.jpg" alt="package" width="70" />
-        </Col>
+class NotificationItem extends Component {
+  state = {
+    viewDetails: false
+  }
 
-        <Col s={4}>
-          <p><b>{props.notification.subject}</b></p>
-          <p>{props.notification.details}</p>
-        </Col>
+  render() {
+    const { notification } = this.props;
 
-        <Col s={6} className="right-align">
-          <Button
-            className="btn-spacing"
-            waves="light">
-            View
-          </Button>
+    return (
+      <Card className="left-align">
+        <Row>
+          <Col s={2}>
+            <img src="images/package.jpg" alt="package" width="70" />
+          </Col>
 
-          <Button
-            className="red"
-            waves="light">
-            Dismiss
-          </Button>
-        </Col>
-      </Row>
-    </Card>
-  )
+          <Col s={4}>
+            <p><b>{notification.subject}</b></p>
+            <p>{notification.details}</p>
+          </Col>
+
+          <Col s={6} className="right-align">
+            <Button
+              onClick={() => this.setState({ viewDetails: !this.state.viewDetails })}
+              className="btn-spacing"
+              waves="light">
+              {this.state.viewDetails ? 'Hide Items' : 'Show Items'}
+            </Button>
+
+            <Button
+              className="red"
+              waves="light">
+              Dismiss
+            </Button>
+          </Col>
+        </Row>
+
+        {
+          this.state.viewDetails ?
+          <Row>
+            <div>
+              <p>Items in this package:</p>
+            {
+              notification.items.map(item => {
+                return <Card key={notification._id}>{item.name}</Card>;
+              })
+            }
+            </div>
+          </Row>
+          : ''
+        }
+      </Card>
+    )
+  }
 }
 
-export default NotificationItem;
+export default withRouter(NotificationItem);
