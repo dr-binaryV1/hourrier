b42e1839537d7b3d725adc676757c96ac0f3802c
dr-binaryV1 <developerdamian@gmail.com>
2017-11-08 21:40:42 -0500

    Instantiate notifications component

--- client/src/components/user/notifications.js
+++ client/src/components/user/notifications.js
@@ -1,14 +1,76 @@
 import React, { Component } from 'react';
-import { Container } from 'react-materialize';
+import { Container, Row, Button } from 'react-materialize';
+import { connect } from 'react-redux';
+import { get_user } from '../../actions';
+
+import { getNotifications } from '../../helpers/api';
+import NotificationItem from './notificationItem';
 
 class Notification extends Component {
+  state = {
+    notifications: null
+  }
+
+  componentDidMount() {
+     this.props.get_user();
+  }
+
+  getNotifs(notificationIds) {
+    getNotifications(notificationIds)
+    .then(res => res.json())
+    .then(res => {
+      this.setState({ notifications: res.notifications });
+    })
+    .catch(err => `Error reported: ${err}`)
+  }
+
   render() {
+    const { user } = this.props;
+    const { notifications } = this.state;
+
+    user ?
+    this.state.notifications === null ?
+    this.getNotifs(user.notificationIds)
+    : '' 
+    : '';
+
     return (
       <Container>
-        <h5>No Notifications at this time</h5>
+        <Row className="right-align">
+          <Button
+            className="search-btn"
+            waves="light">
+            Dismiss All
+          </Button>
+        </Row>
+        {
+          user ?
+          user.notificationIds.length < 1 ?
+          <h5>No Notifications at this time</h5>
+          :
+          notifications ?
+          <div>
+            <p className="left-align important-msg"><i>*You have some stuff that needs attention.</i></p>
+            {
+              notifications.map(notification => {
+                return <NotificationItem notification={notification} key={notification._id} />
+              })
+            }
+          </div>
+          :
+          ''
+          :
+          ''
+        }
       </Container>
     )
   }
 }
 
-export default Notification;
+function mapStateToProps(state) {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps, { get_user })(Notification);
