992d0a6a002c0a28b75295b8d7759ecbf79f95cf
dr-binaryV1 <developerdamian@gmail.com>
2017-11-20 00:44:09 -0500

    Add button and action to delete item

--- client/src/components/admin/orderItem.js
+++ client/src/components/admin/orderItem.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Card, Row, Col, Button } from 'react-materialize';
 import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { delete_order } from '../../actions';
+import Moment from 'moment';
+import AccessTime from 'react-icons/lib/md/access-time';
 
 const OrderItem = (props) => {
   const { order } = props;
@@ -8,7 +12,7 @@ const OrderItem = (props) => {
   return (
     <Card>
       <Row>
-        <Col s={6} className="left-align">
+        <Col s={4} className="left-align">
           <p><b>Order Id:</b> {order._id}</p>
         </Col>
 
@@ -16,15 +20,25 @@ const OrderItem = (props) => {
           <p><b>Status:</b> {order.status}</p>
         </Col>
 
-        <Col s={3} className="right-align">
+        <Col s={5} className="right-align">
           <Button
             onClick={() => props.history.push(`/hourrier-admin/orders/${order._id}`)}
+            className="btn-spacing"
             waves="light">
-          View</Button>
+            View
+          </Button>
+
+          <Button
+            onClick={() => props.delete_order(order._id)}
+            className="red"
+            waves="light">
+            Delete
+          </Button>
+          <p className="right-align small-top-margin"><AccessTime /> {Moment(order.createdAt).fromNow()}</p>
         </Col>
       </Row>
     </Card>
   )
 }
 
-export default withRouter(OrderItem);
+export default withRouter(connect(null, { delete_order })(OrderItem));
