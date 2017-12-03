cfd954ff61de1472c0a1002cc28135baa78a48fa
dr-binaryV1 <developerdamian@gmail.com>
2017-12-03 12:10:02 -0500

    Add Method to update package

--- client/src/components/packages/packageItem.js
+++ client/src/components/packages/packageItem.js
@@ -4,16 +4,37 @@ import { Link } from 'react-router-dom';
 import AccessTime from 'react-icons/lib/md/access-time';
 import Moment from 'moment';
 
-import { getPackage } from '../../helpers/api';
+import { getPackage, packageDelivered, deliveredToKnutsford } from '../../helpers/api';
 
 class PackageItem extends Component {
   state = {
     item: null,
-    viewItems: false
+    viewItems: false,
+
   }
 
   componentDidMount() {
     getPackage(this.props.id)
+    .then(res => res.json())
+    .then(res => {
+      this.setState({ item: res })
+    })
+    .catch(err => console.log(`Error reported: ${err}`))
+  }
+
+  onReceivePackage() {
+    const { item } = this.state;
+    packageDelivered(item._id)
+    .then(res => res.json())
+    .then(res => {
+      this.setState({ item: res })
+    })
+    .catch(err => console.log(`Error reported: ${err}`))
+  }
+
+  deliverToKnutsford() {
+    const { item } = this.state;
+    deliveredToKnutsford(item._id)
     .then(res => res.json())
     .then(res => {
       this.setState({ item: res })
@@ -41,18 +62,40 @@ class PackageItem extends Component {
           <Col s={6} className="center-align">
             <p><b>Status: </b>{item ? item.status: ''}</p>
             <p><b>Details: </b> This Package contains {item ? item.items.length: 0} Item(s)</p>
-            <Button
-              className="green"
-              waves='light'>
-              Package Received
-            </Button>
+
+            {
+              item ?
+              item.status === 'Package Received' ?
+              <Button
+                onClick={this.deliverToKnutsford.bind(this)}
+                className="blue"
+                waves='light'>
+                Delivered to Knutsford Express
+              </Button>
+              :
+              item.status === 'Delivered to Knutsford' ?
+              <Button
+                onClick={this.onReceivePackage.bind(this)}
+                className="red"
+                waves='light'>
+                Dismiss Package
+              </Button>
+              :
+              <Button
+                // onClick={this.onReceivePackage.bind(this)}
+                className="green"
+                waves='light'>
+                Package Received
+              </Button>
+              : ''
+            }
           </Col>
 
           <Col s={3} className="right-align">
             <Button
               onClick={() => this.setState({ viewItems: !this.state.viewItems })}
               waves='light'>
-              {this.state.viewItems ? 'Hide Items' : 'Show Items'}
+              {this.state.viewItems ? 'Hide Item(s)' : 'Show Item(s)'}
             </Button>
           </Col>
         </Row>
