3efc7ce12d107c5472c19d88cc5f73f24631ca25
dr-binaryV1 <developerdamian@gmail.com>
2017-12-09 12:09:54 -0500

    Create knutdford component

---
+++ client/src/components/knutsford-admin/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Container } from 'react-materialize';
+
+class KnutsfordDashboard extends Component {
+  render() {
+    return (
+      <Container></Container>
+    )
+  }
+}
+
+export default KnutsfordDashboard;
