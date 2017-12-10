10c4190171a5250d9670603c76697dd8b59f3240
Damian Wynter <damianwynter@JANICE-QualityWorkss-MacBook-Pro.local>
2017-12-10 00:44:17 -0500

    Flesh out knutsford dashboard

--- client/src/components/knutsford-admin/index.js
+++ client/src/components/knutsford-admin/index.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import { Container } from 'react-materialize';
 
+import { getKnutsfordItems } from '../../helpers/api';
+
 class KnutsfordDashboard extends Component {
+  state = {
+    orders: [],
+    loading: false
+  }
+
+  componentDidMount() {
+    getKnutsfordItems()
+    .then(res => res.json())
+    .then(res => {
+      this.setState({ orders: res.orders })
+    })
+    .catch(err => console.log(`Error reported: ${err}`))
+  }
+
   render() {
     return (
-      <Container></Container>
+      <Container>
+        <h5>Knutsford Panel</h5>
+      </Container>
     )
   }
 }
