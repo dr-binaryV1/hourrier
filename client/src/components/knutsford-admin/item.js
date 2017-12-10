96b03ab04305e01831b90e1afaf54c147b0e4873
Damian Wynter <damianwynter@JANICE-QualityWorkss-MacBook-Pro.local>
2017-12-10 01:58:22 -0500

    minor changes

--- client/src/components/knutsford-admin/item.js
+++ client/src/components/knutsford-admin/item.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { Card, Row, Col, Button } from 'react-materialize';
 
 const Item = props => {
+  const { order } = props;
+  
   return (
     <Card>
-      
+      <Row>
+        <Col>
+          <p><b>Id: </b>{order._id}</p>
+        </Col>
+      </Row>
     </Card>
   )
 };
