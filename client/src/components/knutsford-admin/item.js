3e32e3df1aca59c89f36e4fafb7453f6f24dd6b4
Damian Wynter <damianwynter@JANICE-QualityWorkss-MacBook-Pro.local>
2017-12-10 00:43:55 -0500

    Add Item to knutsford panel

---
+++ client/src/components/knutsford-admin/item.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, Row, Col, Button } from 'react-materialize';
+
+const Item = props => {
+  return (
+    <Card>
+      
+    </Card>
+  )
+};
+
+export default Item;
