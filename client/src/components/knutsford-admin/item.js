e017bc85d14d9af9b489bf1498df2abfb55e9dd7
Damian Wynter <developerdamian@gmail.com>
2017-12-25 11:38:56 -0500

    Remove unnecessary calls to API

--- client/src/components/knutsford-admin/item.js
+++ client/src/components/knutsford-admin/item.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Card, Row, Col, Button } from 'react-materialize';
+import { Link } from 'react-router-dom';
 import Moment from 'moment';
 import AccessTime from 'react-icons/lib/md/access-time';
-import { getPackage, getUser, getItinerary } from '../../helpers/api';
+import { getKnutsordData, getPackage, getUser, getItinerary } from '../../helpers/api';
 
 class Item extends Component {
   state = {
     loading: false,
-    package: null,
+    viewItems: false,
+    pkg: null,
     traveler: null,
     itinerary: null
   }
@@ -18,30 +20,17 @@ class Item extends Component {
 
   getInfo() {
     const { order } = this.props;
-    getPackage(order.packageId)
+    getKnutsordData(order.packageId)
       .then(res => res.json())
       .then(res => {
-        this.setState({ pkg: res });
-        getUser(res.travelerId)
-          .then(res => res.json())
-          .then(res => {
-            this.setState({ traveler: res })
-            const data = { itineraryIds: res.itineraryIds };
-            getItinerary(data)
-              .then(res => res.json())
-              .then(res => {
-                this.setState({ itinerary: res.travelItinerary[0] })
-              })
-              .catch(err => console.log(`Error reported: ${err}`))
-          })
-          .catch(err => console.log(`Error reported: ${err}`))
+        this.setState({ pkg: res.pkg, traveler: res.usr, itinerary: res.itinerary })
       })
-      .catch(err => console.log(`Error reported: ${err}`))
+      .catch(err => console.log(`Error reported ${err}`));
   }
 
   render() {
     const { order } = this.props;
-    const { pkg, traveler, itinerary } = this.state;
+    const { pkg, traveler, itinerary, viewItems } = this.state;
 
     return (
       <Card>
@@ -59,33 +48,33 @@ class Item extends Component {
           </Col>
         </Row>
 
-        { traveler ?
+        {traveler ?
           <Row>
             <Col s={3} className="left-align">
               <p><b>Traveler Name: </b>{traveler.firstname} {traveler.lastname}</p>
             </Col>
 
-            { itinerary ?
-            <div>
-              <Col s={3}>
-                <p><b>Arrival City: </b>{itinerary.arrivalCity}</p>
-              </Col> 
+            {itinerary ?
+              <div>
+                <Col s={3}>
+                  <p><b>Arrival City: </b>{itinerary.arrivalCity}</p>
+                </Col>
 
-              <Col s={3}>
-                <p><b>Arrival Date: </b>{new Date(itinerary.arrivalDate).toLocaleDateString()}</p>
-              </Col>
+                <Col s={3}>
+                  <p><b>Arrival Date: </b>{new Date(itinerary.arrivalDate).toLocaleDateString()}</p>
+                </Col>
 
-              <Col s={3}>
-                <p><b>Arrival Time: </b>{itinerary.arrivalTime}</p>
-              </Col>
-            </div>
-            : ''
+                <Col s={3}>
+                  <p><b>Arrival Time: </b>{itinerary.arrivalTime}</p>
+                </Col>
+              </div>
+              : ''
             }
           </Row>
           : ''
         }
 
-        { pkg ?
+        {pkg ?
           <Row>
             <Col s={4} className="left-align">
               <p><b>Package Status: </b>{pkg.status}</p>
@@ -93,11 +82,37 @@ class Item extends Component {
 
             <Col s={4}>
               <Button
+                onClick={() => this.setState({ viewItems: !viewItems })}
                 waves="light">
-                View Items
+                { viewItems ? 'Hide Items' : 'View Items'}
+              </Button>
+            </Col>
+
+            <Col s={4} className="right-align">
+              <Button
+                //onClick={() => this.setState({ viewItems: !viewItems })}
+                className="green"
+                waves="light">
+                Confirm Package
               </Button>
             </Col>
           </Row>
+          : ''
+        }
+
+        {
+          viewItems ?
+            pkg.items.map(item => {
+              return <Card key={item._id}>
+                <Row>
+                  <Col s={1} className="left-align">
+                    <img src={item.image} alt="product logo" width="50" />
+                  </Col>
+                  <Col s={8}>{item.name}</Col>
+                  <Col s={3} className="right-align"><Link to={item.url} target="_blank">View Product</Link></Col>
+                </Row>
+              </Card>;
+            })
           : ''
         }
       </Card>
