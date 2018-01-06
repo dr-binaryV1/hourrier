15828cec7771551a750ecec66ba8e44e35a876c2
Damian Wynter <dwynter@qualityworkscg.com>
2018-01-06 17:20:46 -0500

    Add information for dropdown lists

---
+++ client/src/utils/filterInfo.js
@@ -0,0 +1,3 @@
+module.exports.filterBy = ['Name', 'Traveler Arrival Destination', 'Traveler Arrival Date'];
+module.exports.arrivalCity = ['Kingston', 'Montego Bay'];
+module.exports.arrivalDate = ['Today', 'Tomorrow', '3 Days', '5 Days'];
