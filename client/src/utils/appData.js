f9b0dabfd253f1411f724ceeed57e23bb8411e13
Damian Wynter <dwynter@qualityworkscg.com>
2018-01-06 21:03:28 -0500

    Rename File

---
+++ client/src/utils/appData.js
@@ -0,0 +1,20 @@
+module.exports.filterBy = ['Name', 'Traveler Arrival Destination', 'Traveler Arrival Date'];
+module.exports.arrivalCity = ['Kingston', 'Montego Bay'];
+module.exports.arrivalDate = ['Today', 'Tomorrow', '3 Days', '5 Days'];
+module.exports.knutsfordLocation = [
+  'Annotto Bay',
+  'Gutters',
+  'GT Extravaganza',
+  'Falmouth',
+  'Luana',
+  'Sangsters Intl Airport',
+  'Montego Bay',
+  'Norman Manley Intl Airport',
+  'Kingston',
+  'Negril',
+  'Ocho Rios',
+  'Port Maria',
+  'Port Antonio',
+  'Mandeville',
+  'Savanna La Mar'
+];
