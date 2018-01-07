30184d710f90261c320d87a0b1717a1b99fbb245
Damian Wynter <dwynter@qualityworkscg.com>
2018-01-06 21:03:35 -0500

    Rename File

--- client/src/utils/filterInfo.js
+++
@@ -1,3 +0,0 @@
-module.exports.filterBy = ['Name', 'Traveler Arrival Destination', 'Traveler Arrival Date'];
-module.exports.arrivalCity = ['Kingston', 'Montego Bay'];
-module.exports.arrivalDate = ['Today', 'Tomorrow', '3 Days', '5 Days'];
